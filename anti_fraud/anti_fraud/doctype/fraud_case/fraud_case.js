e1dd28b26d2f6cfb4a556d12ca0842e3fa7c28c6
AhmedRamzi22 <ahmed.ramzi222@gmail.com>
2025-01-29 15:30:07 +0200

    update fields and migrate data

--- anti_fraud/anti_fraud/doctype/fraud_case/fraud_case.js
+++ anti_fraud/anti_fraud/doctype/fraud_case/fraud_case.js
@@ -5,19 +5,41 @@ frappe.ui.form.on("Fraud Case", {
   refresh: function (frm) {
     frm.events.set_fraud_incident_btn(frm);
     frm.events.set_cs_team_btn(frm);
+    // TODO: to be remove
+    frm.events.migrate_date(frm);
+  },
+  migrate_date: function (frm) {
+    frm.add_custom_button("Migrate Data", () => {
+      frm.call({
+        method: "migrate_data",
+        doc: frm.doc,
+        callback: function (r) {},
+      });
+    });
   },
   set_fraud_incident_btn: function (frm) {
-    if (
-      !frm.is_new() &&
-      frm.doc.docstatus == 1 &&
-      frm.doc.the_relevant_department == "Anti – Fraud Unit"
-    ) {
-      frm.add_custom_button("Create Fraud Incident", () => {
-        frappe.new_doc("Fraud Incident", {
-          case_fraud: cur_frm.doc.name,
-        });
+    frappe.db
+
+      .get_value("Fraud Incident", { case_fraud: frm.doc.name }, "name")
+      .then((r) => {
+        if (!r.message.name) {
+          if (
+            !frm.is_new() &&
+            frm.doc.docstatus == 1 &&
+            frm.doc.the_relevant_department == "Anti – Fraud Unit"
+          ) {
+            frm.add_custom_button("Create Fraud Incident", () => {
+              frappe.new_doc("Fraud Incident", {
+                case_fraud: cur_frm.doc.name,
+                customer: cur_frm.doc.customer,
+                customer_name: cur_frm.doc.customer_name,
+                customer_phone_number: cur_frm.doc.customer_phone_number,
+                loss_amount: cur_frm.doc.loss_amount,
+              });
+            });
+          }
+        }
       });
-    }
   },
   is_a_fraud_case_opened: function (frm) {
     if (frm.doc.is_a_fraud_case_opened == "Yes") {
--- anti_fraud/anti_fraud/doctype/fraud_case/fraud_case.py
+++ anti_fraud/anti_fraud/doctype/fraud_case/fraud_case.py
@@ -1,8 +1,60 @@
 # Copyright (c) 2024, bti and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.model.naming import make_autoname
 class FraudCase(Document):
-	pass
+    
+    @frappe.whitelist()
+    def migrate_data(self):
+        cases=frappe.db.get_all("Fraud Case",filters={"docstatus":1},pluck="name")
+        if len(cases):
+                for case in cases:
+                    incident= frappe.db.exists("Fraud Incident", {"case_fraud": case})
+                    if incident:
+                        customer,customer_name,customer_phone_number,loss_amount=frappe.db.get_value("Fraud Case",case,["customer","customer_name","customer_phone_number","loss_amount"])
+                        frappe.db.set_value("Fraud Incident",incident,"customer",customer)
+                        frappe.db.set_value("Fraud Incident",incident,"customer_name",customer_name)
+                        frappe.db.set_value("Fraud Incident",incident,"customer_phone_number",customer_phone_number)
+                        frappe.db.set_value("Fraud Incident",incident,"loss_amount",loss_amount)
+                        
+        incidents = frappe.db.get_all("Fraud Incident", filters={"docstatus": 1}, pluck="name")
+
+        if incidents:
+                for incident in incidents:
+                    incident_doc = frappe.get_doc("Fraud Incident", incident)
+
+                    if incident_doc.suspect:
+                        investigation = frappe.db.exists("Fraud Investigation", {"fraud_incident": incident})
+
+                        if investigation:
+                           
+                            investigation_doc = frappe.get_doc("Fraud Investigation", investigation)
+                            if not len(investigation_doc.suspect):
+                                for i in incident_doc.suspect:
+                                    child_name = make_autoname("FIS-.#####")
+                                    frappe.db.sql("""
+                                                INSERT INTO `tabFraud Incident Suspect` 
+                                                (`name`, `parent`, `parentfield`, `parenttype`, `suspect_name`, `phone_number`, `civil_id`, `nationality`, `fraud_list`, `idx`, `docstatus`)
+                                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                                            """, (
+                                                child_name, 
+                                                investigation_doc.name,  
+                                                "suspect",  
+                                                "Fraud Investigation",  
+                                                i.suspect_name,
+                                                i.phone_number,
+                                                i.civil_id,
+                                                i.nationality,
+                                                i.fraud_list,
+                                                len(investigation_doc.suspect) + 1, 
+                                                1  
+                                            ))
+
+                            frappe.db.commit()
+
+                                
+        
+        
+        frappe.msgprint("Data Migrated Successfully")
